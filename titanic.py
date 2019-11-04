e58c7b8a066de8bc866cca8105e615a0a39fc2ed
Rohan Bhanot <rohanbhanot@Rohans-MacBook-Pro-2.local>
2019-11-04 12:52:26 -0800

    data cleaning def

--- titanic.py
+++ titanic.py
@@ -40,38 +40,38 @@ as an example of what a submission file should look like.
 # > Data splicing
 # Gender
 sex_data = train_df["Sex"].value_counts()
-print("Gender: ")
-print(sex_data)
-print()
+# print("Gender: ")
+# print(sex_data)
+# print()
 
 # Age
 age_data = train_df["Age"].value_counts()
 age_range = train_df["Age"].max() - train_df["Age"].min()
-print("Age: ")
-print(age_data.head())
-print()
-print(f"The range in the age is {age_range}")
-print()
+# print("Age: ")
+# print(age_data.head())
+# print()
+# print(f"The range in the age is {age_range}")
+# print()
 
 # Siblings & Spouses aboard
 sibsp_data = train_df["SibSp"].value_counts()
-print("Siblings & Spouses: ")
-print(sibsp_data)
-print()
+# print("Siblings & Spouses: ")
+# print(sibsp_data)
+# print()
 
 # Parents & Children aboard
 parch_data = train_df["Parch"].value_counts()
-print("Parents & Children: ")
-print(parch_data)
-print()
+# print("Parents & Children: ")
+# print(parch_data)
+# print()
 
 # Ticket class
 pclass_data = train_df["Pclass"].value_counts()
 pclasses = list(train_df["Pclass"].value_counts().index)
-print("Ticket Class: ")
-print(pclass_data)
-print(f"The different classes are: {pclasses}")
-print()
+# print("Ticket Class: ")
+# print(pclass_data)
+# print(f"The different classes are: {pclasses}")
+# print()
 
 # Ticket cost
 fare_data = train_df["Fare"].value_counts()
@@ -79,70 +79,89 @@ different_tickets = len(train_df["Fare"].value_counts().index)
 ticket_max = train_df["Fare"].max()
 ticket_min = train_df["Fare"].min()
 ticket_range = ticket_max - ticket_min
-print("Ticket Cost: ")
-print(fare_data.head())
-print()
-print(f"There were {different_tickets} different types of tickets to buy, \n"
-      f">>> The most expensive: ${ticket_max}\n"
-      f">>> The least expensive: ${ticket_min}\n"
-      f">> The range: {ticket_range}")
-print()
+# print("Ticket Cost: ")
+# print(fare_data.head())
+# print()
+# print(f"There were {different_tickets} different types of tickets to buy, \n"
+#       f">>> The most expensive: ${ticket_max}\n"
+#       f">>> The least expensive: ${ticket_min}\n"
+#       f">> The range: {ticket_range}")
+# print()
 
 # Cabin info
 cabin_data = train_df["Cabin"].value_counts()
 different_cabins = len(train_df["Cabin"].value_counts().index)
-print("Cabin Info: ")
-print(cabin_data.head())
-print(f"There are {different_cabins} number of cabins.")
-print()
+# print("Cabin Info: ")
+# print(cabin_data.head())
+# print(f"There are {different_cabins} number of cabins.")
+# print()
 
 # > New df with relevant columns
-features = ["PassengerId", "Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Cabin"]
+features = ["PassengerId", "Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
 titanic_df = train_df[features]
 print("Simplified df: ")
 print(titanic_df.head())
 print()
 print(titanic_df.count())
+print()
 
 # > Visualizing data
-# plt.subplots(2, 2, constrained_layout=True)
-plt.subplot2grid((2, 3), (0, 0))
-train_df["Survived"].value_counts(normalize=True).plot(kind="bar", alpha=0.5)
-plt.title("Survived")
-
-plt.subplot2grid((2, 3), (0, 1))
-plt.scatter(train_df["Survived"], train_df["Age"], alpha=0.1)
-plt.title("Survived vs. Age")
-
-plt.subplot2grid((2, 3), (0, 2))
-train_df["Pclass"].value_counts(normalize=True).plot(kind="bar", alpha=0.5)
-plt.title("Class")
+# plt.subplot2grid((2, 3), (0, 0))
+# train_df["Survived"].value_counts(normalize=True).plot(kind="bar", alpha=0.5)
+# plt.title("Survived")
+#
+# plt.subplot2grid((2, 3), (0, 1))
+# plt.scatter(train_df["Survived"], train_df["Age"], alpha=0.1)
+# plt.title("Survived vs. Age")
+#
+# plt.subplot2grid((2, 3), (0, 2))
+# train_df["Pclass"].value_counts(normalize=True).plot(kind="bar", alpha=0.5)
+# plt.title("Class")
 
 # plt.subplot2grid((2, 3), (1, 0), colspan=2)
 # for x in [1, 2, 3]:
 #       train_df[["Age", "Pclass"]].groupby("Pclass").plot(kind="kde")
 # plt.title("Class vs. Age")
 
-plt.subplot2grid((2, 3), (1, 0))
-plt.scatter(train_df["Survived"], train_df["Pclass"], alpha=0.1)
-plt.title("Survived vs. Class")
-
-plt.subplot2grid((2, 3), (1, 1))
-plt.scatter(train_df["Age"], train_df["Pclass"], alpha=0.1)
-plt.title("Age vs. Class")
-
-plt.subplot2grid((2, 3), (1, 2))
-plt.scatter(train_df["Survived"], train_df["SibSp"], alpha=0.1)
-plt.title("Survived vs. Num Siblings/Spouses")
-
-
-plt.tight_layout()
-plt.show()
+# plt.subplot2grid((2, 3), (1, 0))
+# plt.scatter(train_df["Survived"], train_df["Pclass"], alpha=0.1)
+# plt.title("Survived vs. Class")
+#
+# plt.subplot2grid((2, 3), (1, 1))
+# plt.scatter(train_df["Age"], train_df["Pclass"], alpha=0.1)
+# plt.title("Age vs. Class")
+#
+# plt.subplot2grid((2, 3), (1, 2))
+# plt.scatter(train_df["Survived"], train_df["SibSp"], alpha=0.1)
+# plt.title("Survived vs. Num Siblings/Spouses")
+#
+#
+# plt.tight_layout()
+# plt.show()
 
 # > Replacing non-numeric variables with numeric values
-cabin_fill = titanic_df["Cabin"].values
+# > Simple prediction
+train_df["Hypothesis"] = 0
+train_df.loc[train_df.Sex == "female", "Hypothesis"] = 1
 
+train_df["Result"] = 0
+train_df.loc[train_df.Survived == train_df["Hypothesis"], "Result"] = 1
+
+print(train_df["Result"].value_counts(normalize=True))
 
 def clean_data(data):
       data["Age"] = data["Age"].fillna(data["Age"].dropna().median())
+      data["Fare"] = data["Fare"].fillna(data["Fare"].dropna().median())
+
+      data.loc[data["Sex"] == "male", "Sex"] = 0
+      data.loc[data["Sex"] == "female", "Sex"] = 1
+
+      data["Embarked"] = data["Embarked"].fillna("S")
+      data.loc[data["Embarked"] == "S", "Embarked"] = 0
+      data.loc[data["Embarked"] == "C", "Embarked"] = 1
+      data.loc[data["Embarked"] == "Q", "Embarked"] = 2
+
+
+
+
 
